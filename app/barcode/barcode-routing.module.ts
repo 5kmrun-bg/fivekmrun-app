732d4c5a0a3843803a8f1bb23ac795830ac31c0a
Emil Tabakov <emil.tabakov@gmail.com>
2017-08-24 21:35:00 +0300

    Create navigation drawer

--- app/barcode/barcode-routing.module.ts
+++ app/barcode/barcode-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from "@angular/core";
 import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 
-import { FeaturedComponent } from "./featured.component";
+import { BarcodeComponent } from "./barcode.component";
 
 const routes: Routes = [
-    { path: "", component: FeaturedComponent }
+    { path: "", component: BarcodeComponent }
 ];
 
 @NgModule({
     imports: [NativeScriptRouterModule.forChild(routes)],
     exports: [NativeScriptRouterModule]
 })
-export class FeaturedRoutingModule { }
+export class BarcodeRoutingModule { }
