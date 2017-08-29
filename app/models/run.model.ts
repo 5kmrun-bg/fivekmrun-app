afc361874404adf731aad6fffc4886ff1443b102
Emil Tabakov <emil.tabakov@gmail.com>
2017-08-29 17:33:22 +0300

    Create artifical id for the runs

--- app/models/run.model.ts
+++ app/models/run.model.ts
@@ -1,10 +1,15 @@
+import { Md5 } from "ts-md5/dist/md5";
+
 export class Run {
+    public id: string;
+
     constructor(
-        public id: number,
         public date: Date,
         public time: string,
         public place: string,
         public differenceFromPrevious: number,
         public differenceFromBest: number
-    ) {}
+    ) {
+        this.id = Md5.hashStr(date + "#" + time + "#" + place + "#").toString();
+    }
 }
