2a8cbf2f01f630ea66e40cc7c36fa80a492e95b4
vakrilov <alexander.vakrilov@telerik.com>
2017-10-14 00:52:27 +0300

    Fix charton iOS

--- app/home/tiles/run-stats-tile/run-stats-tile.component.ts
+++ app/home/tiles/run-stats-tile/run-stats-tile.component.ts
@@ -2,14 +2,24 @@ import { Component, OnInit, Input } from "@angular/core";
 import { Run } from "../../../models";
 import { Observable } from "rxjs/Observable";
 
-@Component({ 
+@Component({
     selector: "run-stats-tile",
     moduleId: module.id,
     templateUrl: "./run-stats-tile.component.html"
 })
 export class RunStatsTileComponent implements OnInit {
     @Input() runs$: Observable<Run[]>;
-    
+
+    min: number;
+    max: number;
+    step: number;
+
     ngOnInit(): void {
+        this.runs$.do((runs) => {
+            const times = runs.map(r => r.timeInSeconds);
+            this.max = Math.ceil(Math.max(...times));
+            this.min = Math.floor(Math.min(...times));
+            this.step = Math.round((this.max - this.min) / 4);
+        }).subscribe();
     }
 }
