73f46c79635e015e0c83b7b97f579f656dda1647
Emil Tabakov <emil.tabakov@gmail.com>
2018-05-22 13:51:14 +0300

    refactor: remove fabric

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,8 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 import { AppModule } from "./app.module";
-import { Fabric } from 'nativescript-fabric';
 
 import * as elementRegistryModule from 'nativescript-angular/element-registry';
 elementRegistryModule.registerElement("CardView", () => require("nativescript-cardview").CardView);
-
-Fabric.init();
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
