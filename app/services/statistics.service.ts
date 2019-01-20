8f58ca4a267afd054eb2ea8389052db373735323
Emil Tabakov <emil.tabakov@gmail.com>
2019-01-20 16:54:01 +0200

    style the bars a bit

--- app/services/statistics.service.ts
+++ app/services/statistics.service.ts
@@ -23,7 +23,7 @@ export class StatisticsService {
         return this.runService.getByCurrentUser()
                                 .map(runs => {
                                     const cityRuns = (new List(runs)).GroupBy(r => r.place, r => r.timeInSeconds);
-                                    const result = Object.keys(cityRuns).map(k => {return {City:this.trimCityName(k), BestTime: Math.round(Math.min.apply(null, cityRuns[k]) * 100)/100}});
+                                    const result = Object.keys(cityRuns).map(k => {return {City: k, BestTime: Math.round(Math.min.apply(null, cityRuns[k]) * 100)/100}});
 
                                     return result;
                                 })
