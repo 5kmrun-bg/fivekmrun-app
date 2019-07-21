dfdacd243142f87bee80e06772a5a13b5d835b13
vakrilov <alexander.vakrilov@telerik.com>
2019-07-21 17:34:03 +0300

    feat:navigate from tile to run directly

--- app/tabs/tabs.component.ts
+++ app/tabs/tabs.component.ts
@@ -1,16 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild, AfterViewInit, ElementRef, OnInit } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
 import { ActivatedRoute } from "@angular/router";
+import { NavigationService } from "../services/navigation.service";
+import { TabView } from "tns-core-modules/ui/tab-view";
 
 @Component({
     moduleId: module.id,
     selector: "tabs-page",
     templateUrl: "./tabs.component.html"
 })
-export class TabsComponent {
+export class TabsComponent implements OnInit, AfterViewInit {
+    @ViewChild("tabView", { static: true })
+    tabElementRef: ElementRef<TabView>;
+
     constructor(
         private routerExtension: RouterExtensions,
-        private activeRoute: ActivatedRoute) {
+        private activeRoute: ActivatedRoute,
+        private navService: NavigationService
+    ) {
     }
 
     ngOnInit() {
@@ -22,5 +29,9 @@ export class TabsComponent {
                 resultsTab: ["results"]
             }
         }], { relativeTo: this.activeRoute });
+    }
+
+    ngAfterViewInit() {
+        this.navService.initTabView(this.tabElementRef.nativeElement);
     }
 }
