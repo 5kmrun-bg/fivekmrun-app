86879a5fd856acf3af796685bfd441ddbc1df0ec
Emil Tabakov <emil.tabakov@gmail.com>
2019-08-23 10:13:17 +0300

    feat: add donation tab

---
+++ app/donation/donation.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, ViewChild, ElementRef} from "@angular/core";
+
+@Component({
+    selector: "Donation",
+    moduleId: module.id,
+    templateUrl: "./donation.component.html"
+})
+export class DonationComponent implements OnInit {
+    ngOnInit(): void {
+    }
+}
