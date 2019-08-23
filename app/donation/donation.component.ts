f487967d7e33bdf7340a18d9cfd4d5e23e652838
Emil Tabakov <emil.tabakov@gmail.com>
2019-08-23 15:30:32 +0300

    feat: add firebase tracking for the donations

--- app/donation/donation.component.ts
+++ app/donation/donation.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewChild, ElementRef} from "@angular/core";
+import { Component, OnInit} from "@angular/core";
+import * as utils from "tns-core-modules/utils/utils";
+import * as firebase from "nativescript-plugin-firebase";
 
 @Component({
     selector: "Donation",
@@ -7,5 +9,12 @@ import { Component, OnInit, ViewChild, ElementRef} from "@angular/core";
 })
 export class DonationComponent implements OnInit {
     ngOnInit(): void {
+        firebase.analytics.logEvent({ key: "page_donation_opened" });
+    }
+
+    initiateDonation(): void {
+        console.log("log donation");
+        firebase.analytics.logEvent({ key: "button_donation_clicked" });
+        utils.openUrl("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6B7VDX7UW7JKJ&source=url");
     }
 }
