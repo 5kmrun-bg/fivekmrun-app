f7873d354130b20a8bebad240a1ed69505a15a95
Emil Tabakov <emil.tabakov@gmail.com>
2019-11-03 17:11:29 +0200

    feat: set up firebase ACM

--- app/app.component.ts
+++ app/app.component.ts
@@ -45,5 +45,28 @@ export class AppComponent implements OnInit {
         this.userService.userChanged.subscribe(() => {
             this.currentUser$ = this.userService.getCurrentUser();
         });
+
+        firebase.init({
+            showNotifications: true,
+            showNotificationsWhenInForeground: true,
+            onPushTokenReceivedCallback: function(token) {
+              console.log("[Firebase] onPushTokenReceivedCallback: " + token);
+            },
+            onMessageReceivedCallback: function(message) {
+                console.log("Title: " + message.title);
+                console.log("Body: " + message.body);
+                // if your server passed a custom property called 'foo', then do this:
+                console.log("Value of 'foo': " + message.data.foo);
+              }
+          }).then(
+            (instance) => {
+              console.log("firebase.init done");
+            }).catch(
+            (error) => {
+              console.log(`firebase.init error: ${error}`);
+            }
+          );
+        
+          firebase.subscribeToTopic("news").then(() => console.log("Subscribed to topic"));
     }
 }
--- app/main.ts
+++ app/main.ts
@@ -6,20 +6,6 @@ import * as firebase from "nativescript-plugin-firebase";
 import * as elementRegistryModule from 'nativescript-angular/element-registry';
 elementRegistryModule.registerElement("CardView", () => require("nativescript-cardview").CardView);
 
-firebase.init({
-    onMessageReceivedCallback: function(message) {
-        console.log("Title: " + message.title);
-        console.log("Body: " + message.body);
-        // if your server passed a custom property called 'foo', then do this:
-        console.log("Value of 'foo': " + message.data.foo);
-      }
-  }).then(
-    (instance) => {
-      console.log("firebase.init done");
-    },
-    (error) => {
-      console.log(`firebase.init error: ${error}`);
-    }
-  );
 
+    
 platformNativeScriptDynamic().bootstrapModule(AppModule);
