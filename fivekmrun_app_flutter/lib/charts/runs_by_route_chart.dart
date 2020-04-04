7e2641f9ce9454bac5bd9b2ff957a3a146081c79
Emil Tabakov <emil.tabakov@gmail.com>
2020-04-04 16:48:00 +0300

    feat: add runs by route chart on profile

---
+++ fivekmrun_app_flutter/lib/charts/runs_by_route_chart.dart
@@ -0,0 +1,67 @@
+import 'dart:math';
+
+import 'package:fivekmrun_flutter/state/run_model.dart';
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import "package:collection/collection.dart";
+
+
+class RunsByRouteChart extends StatelessWidget {
+
+  final List<charts.Series> seriesList;
+  final bool animate;
+
+  RunsByRouteChart(this.seriesList, {this.animate});
+
+  factory RunsByRouteChart.withRuns(List<Run> runs) {
+    return new RunsByRouteChart(
+      _createData(runs),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[IntrinsicHeight(child: Text("Бягания по трасета")), 
+        Expanded(child: new charts.PieChart(seriesList, animate: animate, behaviors: [
+          new charts.DatumLegend(
+          // Positions for "start" and "end" will be left and right respectively
+          // for widgets with a build context that has directionality ltr.
+          // For rtl, "start" and "end" will be right and left respectively.
+          // Since this example has directionality of ltr, the legend is
+          // positioned on the right side of the chart.
+          position: charts.BehaviorPosition.end,
+          // For a legend that is positioned on the left or right of the chart,
+          // setting the justification for [endDrawArea] is aligned to the
+          // bottom of the chart draw area.
+          //outsideJustification: charts.OutsideJustification.endDrawArea,
+          // By default, if the position of the chart is on the left or right of
+          // the chart, [horizontalFirst] is set to false. This means that the
+          // legend entries will grow as new rows first instead of a new column.
+          horizontalFirst: false,
+          // This defines the padding around each legend entry.
+          cellPadding: new EdgeInsets.only(right: 4.0, bottom: 4.0),
+          )
+    ])),
+    ]);
+  }
+
+  static List<charts.Series<RunsByRouteEntry, String>> _createData(List<Run> runs) {
+    List<RunsByRouteEntry> series = groupBy(runs, (r) => r.location)
+                                      .entries.map((e) => RunsByRouteEntry(e.key, e.value.length)).toList();
+
+    return [
+      new charts.Series<RunsByRouteEntry, String>(
+        id: 'BestTimeByRoute',
+        domainFn: (RunsByRouteEntry run, _) => run.location,
+        measureFn: (RunsByRouteEntry run, _) => run.timeInSeconds,
+        data: series,
+      )
+    ];
+  }  
+}
+
+class RunsByRouteEntry {
+  final String location;
+  final int timeInSeconds;
+
+  RunsByRouteEntry(this.location, this.timeInSeconds);
+}
--- fivekmrun_app_flutter/lib/profile.dart
+++ fivekmrun_app_flutter/lib/profile.dart
@@ -1,12 +1,12 @@
+import 'package:fivekmrun_flutter/charts/runs_by_route_chart.dart';
 import 'package:fivekmrun_flutter/common/avatar.dart';
 import 'package:fivekmrun_flutter/common/run_card.dart';
-import 'package:fivekmrun_flutter/state/run_model.dart';
 import 'package:fivekmrun_flutter/state/runs_resource.dart';
 import 'package:fivekmrun_flutter/state/user_resource.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
 
+import 'charts/runs_chart.dart';
 import 'common/milestone.dart';
 
 class ProfileDashboard extends StatelessWidget {
@@ -93,13 +93,13 @@ class ProfileDashboard extends StatelessWidget {
             children: <Widget>[
               Expanded(
                 child: RunCard(
-                  title: "Последно уастие",
+                  title: "Последно участие",
                   run: runsResource.lastRun,
                 ),
               ),
               Expanded(
                 child: RunCard(
-                  title: "Най-добро уастие",
+                  title: "Най-добро участие",
                   run: runsResource.bestRun,
                 ),
               ),
@@ -112,60 +112,15 @@ class ProfileDashboard extends StatelessWidget {
                 child: RunsChart.withRuns(runsResource?.value),
               ),
             ),
+          if (runsResource.value != null)
+            Card(
+              child: Container(
+                height: 200,
+                child: RunsByRouteChart.withRuns(runsResource?.value)
+              ),
+            ),
         ],
       );
     });
-  }
-}
-
-class RunsChart extends StatelessWidget {
-  final List<charts.Series> seriesList;
-  final bool animate;
-
-  RunsChart(this.seriesList, {this.animate});
-
-  /// Creates a [TimeSeriesChart] with sample data and no transition.
-  factory RunsChart.withRuns(List<Run> runs) {
-    return new RunsChart(
-      _createData(runs),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new charts.TimeSeriesChart(seriesList, animate: animate, behaviors: [
-      // Optional - Configures a [LinePointHighlighter] behavior with a
-      // vertical follow line. A vertical follow line is included by
-      // default, but is shown here as an example configuration.
-      //
-      // By default, the line has default dash pattern of [1,3]. This can be
-      // set by providing a [dashPattern] or it can be turned off by passing in
-      // an empty list. An empty list is necessary because passing in a null
-      // value will be treated the same as not passing in a value at all.
-      new charts.LinePointHighlighter(
-          showHorizontalFollowLine:
-              charts.LinePointHighlighterFollowLineType.none,
-          showVerticalFollowLine:
-              charts.LinePointHighlighterFollowLineType.nearest),
-      // Optional - By default, select nearest is configured to trigger
-      // with tap so that a user can have pan/zoom behavior and line point
-      // highlighter. Changing the trigger to tap and drag allows the
-      // highlighter to follow the dragging gesture but it is not
-      // recommended to be used when pan/zoom behavior is enabled.
-      new charts.SelectNearest(eventTrigger: charts.SelectionTrigger.tapAndDrag)
-    ]);
-  }
-
-  /// Create one series with sample hard coded data.
-  static List<charts.Series<Run, DateTime>> _createData(List<Run> runs) {
-    return [
-      new charts.Series<Run, DateTime>(
-        id: 'Runs',
-        // colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-        domainFn: (Run run, _) => run.date,
-        measureFn: (Run run, _) => run.timeInSeconds,
-        data: runs,
-      )
-    ];
   }
 }
