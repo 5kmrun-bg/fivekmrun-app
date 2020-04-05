83e9dec327dbee1421cbb117c56888e9d8a6af55
Emil Tabakov <emil.tabakov@gmail.com>
2020-04-05 20:22:59 +0300

    add circle widget

--- fivekmrun_app_flutter/lib/runs/run_details_page.dart
+++ fivekmrun_app_flutter/lib/runs/run_details_page.dart
@@ -11,30 +11,88 @@ class RunDetailsPage extends StatelessWidget {
     Run run = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-      appBar: AppBar(title: Text(run.displayDate)),
-      body: Column(children: <Widget>[
-        Row(children: <Widget>[
-          Column(children: <Widget>[
-            Text(run.pace.toString()),
-            Text("Темпо"),],), 
-          Column(children: <Widget>[
-            MilestoneGauge(run.position, 400),
-            Text("Позиция"),],),
-          Column(children: <Widget>[
-            Text(run.speed.toString()),
-            Text("Скорост")],),
-        ],),
-        Row(children: <Widget>[
-          Text(new DateFormat("dd.MM.yyyy").format(run.date)),
-          Text(run.time),],),
-        Text(run.location),
-        Row(children: <Widget>[
-          Text("Предишно бягане: "),
-          Text(run.differenceFromPrevious),],),
-        Row(children: <Widget>[
-          Text("Най-добро бягане: "),
-          Text(run.differenceFromBest)],),
-      ],),
+        appBar: AppBar(title: Text(run.displayDate)),
+        body: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 3,
+                    child:
+                  Column(
+                    children: <Widget>[
+                      CircleWidget(run.pace.toString(), "мин/км"),
+                      Text("Темпо"),
+                    ],
+                  )),
+                  Expanded(
+                    flex: 4,
+                    child: Column(
+                    children: <Widget>[
+                      MilestoneGauge(run.position, 400),
+                      Text("Позиция"),
+                    ],
+                  )),
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                    children: <Widget>[
+                      CircleWidget(run.speed.toString(), "км/ч"),
+                      Text("Скорост")
+                    ],
+                  )),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text(new DateFormat("dd.MM.yyyy").format(run.date)),
+                  Text(run.time),
+                ],
+              ),
+              Text(run.location),
+              Row(
+                children: <Widget>[
+                  Text("Предишно бягане: "),
+                  Text(run.differenceFromPrevious),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text("Най-добро бягане: "),
+                  Text(run.differenceFromBest)
+                ],
+              ),
+            ],
+          ),
+        );
+  }
+}
+
+class CircleWidget extends StatelessWidget {
+  final String value;
+  final String measurement;
+
+  CircleWidget(this.value, this.measurement);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(30),
+      child: Column(
+        children: <Widget>[
+          Text(
+            this.value,
+            style: TextStyle(color: Colors.black),
+          ),
+          Text(
+            this.measurement, 
+            style: TextStyle(color: Colors.black))
+        ],
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+      ),
     );
   }
 }
