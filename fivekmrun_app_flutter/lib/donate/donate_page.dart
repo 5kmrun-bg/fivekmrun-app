9f9311ba5752115fdc16e8559fbc2eb2429b9147
Emil Tabakov <emil.tabakov@gmail.com>
2020-05-17 16:49:52 +0300

    fix donations page

--- fivekmrun_app_flutter/lib/donate/donate_page.dart
+++ fivekmrun_app_flutter/lib/donate/donate_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DonatePage extends StatelessWidget {
   const DonatePage({Key key}) : super(key: key);
@@ -10,11 +12,21 @@ class DonatePage extends StatelessWidget {
           leading: BackButton(color: Colors.white),
           title: Text("Дарение")),
         body: Center(
-          child: RaisedButton(
-            child: Text("PayPal"),
-            onPressed: () {
-              // TODO: implement
-            },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Center(child: Text("Това приложение и цялата дейност на 5kmRun се издържа изцяло от дарения и доброволен труд. Можете да подкрепите нашите усилия като дарите избрана от вас сума бързо, лесно и надеждно.", textAlign: TextAlign.center,)),
+              ),
+              RaisedButton(
+                child: Text("Дари с PayPal"),
+                onPressed: () async {
+                  FirebaseAnalytics().logEvent(name: "button_donation_clicked");
+                  await launch("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=U9KNHBAU8VMFS&source=url");
+                },
+              ),
+            ],
           ),
         ));
   }
