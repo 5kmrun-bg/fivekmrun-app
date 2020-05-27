4c1422ac6eac2728f0e7da817242c0a789b8a2a9
Emil Tabakov <emil.tabakov@gmail.com>
2020-05-28 00:28:02 +0300

    fix problem with location outside of a city

--- fivekmrun_app_flutter/lib/state/google_maps_service.dart
+++ fivekmrun_app_flutter/lib/state/google_maps_service.dart
@@ -1,21 +1,21 @@
-import 'dart:convert';
-
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:geocoder/geocoder.dart';
 import 'package:geocoder/services/base.dart';
-import 'package:http/http.dart' as http;
-import 'package:fivekmrun_flutter/constants.dart' as constants;
 import 'package:fivekmrun_flutter/private/secrets.dart';
 
 class GoogleMapsService {
   Future<String> getTownFromGeoLocation(double latitude, double longitude) async {
     try {
-    Geocoding geoCoder = Geocoder.google(googleMapsKey);
-    List<Address> addresses = await geoCoder.findAddressesFromCoordinates(new Coordinates(latitude, longitude));
+      Geocoding geoCoder = Geocoder.google(googleMapsKey);
+      List<Address> addresses = await geoCoder.findAddressesFromCoordinates(new Coordinates(latitude, longitude));
 
-    final result = addresses.where((a) => a.locality != null)?.first?.locality;
-
-    return result;  
+      if (addresses.any((a) => a.locality != null)) {
+        final result = addresses.firstWhere((a) => a.locality != null)?.locality;
+        return result; 
+      } 
+      else {
+        return addresses.firstWhere((a) => a.adminArea != null).adminArea;
+      }
     } on Exception catch(e) {
       Crashlytics.instance.recordError(e, StackTrace.current);
       return "";
