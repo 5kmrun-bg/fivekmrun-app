a1d0df5b8235698c198aabb42e3770702afc9ce2
Emil Tabakov <emil.tabakov@gmail.com>
2021-01-31 16:31:25 +0200

    add padding for smaller devices

--- fivekmrun_app_flutter/lib/barcode_page.dart
+++ fivekmrun_app_flutter/lib/barcode_page.dart
@@ -17,18 +17,23 @@ class BarcodePage extends StatelessWidget {
               title: Text("Barcode"),
               centerTitle: true,),
           backgroundColor: Colors.white,
-          body: Center(child: Column(children: <Widget>[
-            Text(userName, style: TextStyle(color: Colors.black)),
-            BarCodeImage(
-                params: Code39BarCodeParams(
-              userId.toString().padLeft(10, '0'),
-              lineWidth:
-                  2.0, // width for a single black/white bar (default: 2.0)
-              barHeight: 90.0, // height for the entire widget (default: 100.0)
-              withText: false,
-            )),
-            Text(userId.toString(), style: TextStyle(color: Colors.black))
-          ])));
+          body: 
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Column(children: <Widget>[
+                Text(userName, style: TextStyle(color: Colors.black)),
+                BarCodeImage(
+                    params: Code39BarCodeParams(
+                  userId.toString().padLeft(10, '0'),
+                  lineWidth:
+                      2.0, // width for a single black/white bar (default: 2.0)
+                  barHeight: 90.0, // height for the entire widget (default: 100.0)
+                  withText: false,
+                  )),
+                Text(userId.toString(), style: TextStyle(color: Colors.black))
+            ])),
+          ));
     });
   }
 }
