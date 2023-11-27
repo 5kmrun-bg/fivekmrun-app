c34f82327ed7f0736de59d0d1fda87c87a1ada8e
Martin Stoev <martin.y.stoev@gmail.com>
2023-11-27 10:18:30 +0200

    remove snapshot test

--- fivekmrun_app_flutter/test/common/avatar_test.dart
+++ fivekmrun_app_flutter/test/common/avatar_test.dart
@@ -1,8 +1,6 @@
 import 'package:fivekmrun_flutter/common/avatar.dart';
-import 'package:fivekmrun_flutter/login/helpers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:network_image_mock/network_image_mock.dart';
 
 void main() {
@@ -31,50 +29,5 @@ void main() {
     expect(padding.elementAt(0).padding,
         const EdgeInsets.only(top: 12.0, bottom: 12.0));
     expect(padding.elementAt(1).padding, EdgeInsets.all(2));
-  });
-
-  testGoldens('avatar snapshot', (tester) async {
-    final appAccentColor = Color.fromRGBO(218, 3, 56, 1.0);
-    await tester.pumpWidgetBuilder(Builder(
-      builder: (BuildContext context) {
-        return MaterialApp(
-            theme: ThemeData(
-              dividerColor: Colors.black12,
-              textTheme: TextTheme(
-                titleSmall:
-                    TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
-                bodyLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 10),
-                bodyMedium: TextStyle(fontSize: 10),
-              ),
-              outlinedButtonTheme: OutlinedButtonThemeData(style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.resolveWith<Color?>(
-                      (Set<MaterialState> states) {
-                return Colors.white;
-              }))),
-              textButtonTheme: TextButtonThemeData(style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.resolveWith<Color?>(
-                    (Set<MaterialState> states) {
-                  return Colors.white;
-                }),
-              )),
-              appBarTheme: AppBarTheme(
-                  backgroundColor: Color.fromRGBO(66, 66, 66, 1),
-                  iconTheme: IconThemeData(color: Colors.white),
-                  titleTextStyle: Theme.of(context)
-                      .textTheme
-                      .titleLarge
-                      ?.copyWith(color: Colors.white)),
-              colorScheme: ColorScheme.fromSwatch(
-                  primarySwatch: getColor(appAccentColor),
-                  backgroundColor: Colors.black,
-                  accentColor: appAccentColor,
-                  errorColor: Colors.red,
-                  brightness: Brightness.dark),
-            ),
-            home: Avatar(key: new Key('avatar-snapshot'), url: 'snapshot-url'));
-      },
-    ));
-
-    await multiScreenGolden(tester, 'avatar', devices: [Device.phone]);
   });
 }
