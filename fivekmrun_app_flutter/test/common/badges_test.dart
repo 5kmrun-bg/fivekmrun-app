48173d4087a2a2fa6da881fc2a77054add39a7ec
vakrilov <alexander.vakrilov@gmail.com>
2024-01-07 15:57:55 +0200

    fix test

--- fivekmrun_app_flutter/test/common/badges_test.dart
+++ fivekmrun_app_flutter/test/common/badges_test.dart
@@ -14,53 +14,27 @@ void main() {
     expect(hasSelfieBadge(runs), false);
   });
 
-  test('should have max badge this year', () {
+  test('should never have max badge this year', () {
     var runs = <Run>[];
 
     var now = DateTime.now();
-    var saturday = now.lastSaturday();
-    while (saturday.year == now.year) {
-      runs.add(new Run(date: saturday, isSelfie: false));
-      saturday = saturday.subtract(Duration(days: 7));
-    }
-
-    expect(hasMaxBadge(runs), true);
-  });
-
-  test('should not have max badge this year', () {
-    var runs = <Run>[];
-
-    var now = DateTime.now();
-    var saturday = now.lastSaturday().subtract(Duration(days: 7));
-    while (saturday.year == now.year) {
-      runs.add(new Run(date: saturday, isSelfie: false));
-      saturday = saturday.subtract(Duration(days: 7));
+    var date = now;
+    while (date.year == now.year) {
+      runs.add(new Run(date: date, isSelfie: false));
+      date = date.subtract(Duration(days: 1));
     }
 
     expect(hasMaxBadge(runs), false);
   });
 
-  test('should have selfie badge this year', () {
+  test('should never have selfie badge this year', () {
     var runs = <Run>[];
 
     var now = DateTime.now();
-    var saturday = now.lastSaturday();
-    while (saturday.year == now.year) {
-      runs.add(new Run(date: saturday, isSelfie: true));
-      saturday = saturday.subtract(Duration(days: 7));
-    }
-
-    expect(hasSelfieBadge(runs), true);
-  });
-
-  test('should not have selfie badge this year', () {
-    var runs = <Run>[];
-
-    var now = DateTime.now();
-    var saturday = now.lastSaturday().subtract(Duration(days: 7));
-    while (saturday.year == now.year) {
-      runs.add(new Run(date: saturday, isSelfie: true));
-      saturday = saturday.subtract(Duration(days: 7));
+    var date = now;
+    while (date.year == now.year) {
+      runs.add(new Run(date: date, isSelfie: true));
+      date = date.subtract(Duration(days: 1));
     }
 
     expect(hasMaxBadge(runs), false);
@@ -75,7 +49,6 @@ void main() {
       runs.add(new Run(date: saturday, isSelfie: false));
       saturday = saturday.subtract(Duration(days: 7));
     }
-
     expect(hasMaxBadge(runs), true);
   });
 
