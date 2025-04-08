80c9c0d4300354eee22126d6ef44ffec71d95d26
etabakov <emil.tabakov@gmail.com>
2025-04-08 23:01:11 +0300

    a bit of styling

--- fivekmrun_app_flutter/lib/timekeeping/timekeeping.dart
+++ fivekmrun_app_flutter/lib/timekeeping/timekeeping.dart
@@ -7,33 +7,52 @@ class Timekeeping extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text('Времеизмерване компонент'),
-        // Add Chronometer button
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => ChronometerView()),
-            );
-          },
-          child: const Text('Chronometer'),
+    return Expanded(
+      child: Card(
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Времеизмерване',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ChronometerView()),
+                      );
+                    },
+                    icon: const Icon(Icons.timer),
+                    label: const Text('Хронометър'),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => BarcodeScannerView()),
+                      );
+                    },
+                    icon: const Icon(Icons.qr_code_scanner),
+                    label: const Text('Сканер'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-
-        // Add Barcode Scanner button
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => BarcodeScannerView()),
-            );
-          },
-          child: const Text('Barcode Scanner'),
-        ),
-        // Add your timekeeping UI elements here
-      ],
+      ),
     );
   }
 }
